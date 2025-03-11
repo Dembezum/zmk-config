6f1f03e9fc83e33cb1da14530ab039202c26298b
dembezum <dembezuuma@gmail.com>
2025-03-11 17:03:09 +0100

    individual comments

--- config/aliases.h
+++ config/aliases.h
@@ -1,16 +1,16 @@
 // aliases.h
-/* #define DK_plus MINUS
-#define DK_minus FSLH
-#define DK_astrk PIPE
-#define DK_qmark UNDER
-#define DK_caret RBRC
-#define DK_equal RPAR
-#define DK_lpar ASTRK
-#define DK_rpar LPAR
-#define DK_flsh AMPS
-#define DK_sqt BACKSLASH
-#define DK_dqt AT
-#define DK_dllr GRAVE */
+// #define DK_plus MINUS
+// #define DK_minus FSLH
+// #define DK_astrk PIPE
+// #define DK_qmark UNDER
+// #define DK_caret RBRC
+// #define DK_equal RPAR
+// #define DK_lpar ASTRK
+// #define DK_rpar LPAR
+// #define DK_flsh AMPS
+// #define DK_sqt BACKSLASH
+// #define DK_dqt AT
+// #define DK_dllr GRAVE
 
 #define DK_SECT LS(DK_PIPE) // §
 #define DK_PIPE RA(EQUAL)   // |
