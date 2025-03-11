4b7fc06ef619f096fead2655cdc38a262762878d
dembezum <dembezuuma@gmail.com>
2025-03-11 18:34:33 +0100

    s

--- config/aliases.h
+++ config/aliases.h
@@ -1,34 +1,34 @@
 // aliases.h
 // #define DK_flsh AMPS
 
-#define DK_PIPE RA(EQUAL) // |
-#define DK_EXLM LS(N1)    // !
-#define DK_DQUO LS(N2)    // "
-#define DK_HASH LS(N3)    // #
-#define DK_CURR LS(N4)    // ¤
-#define DK_PERC LS(N5)    // %
-#define DK_AMPR LS(N6)    // &
-#define DK_SLSH LS(N7)    // /
-#define DK_LPRN LS(N8)    // (
-#define DK_RPRN LS(N9)    // )
-#define DK_EQL LS(N0)     // =
-// #define DK_QUES LS(DK_MINS) // ?
-#define DK_ASTR PIPE        // *
-#define DK_SQT BACKSLASH    // '
-#define DK_UNDS LS(DK_MINS) // _
-#define DK_MINS SLASH       // -
-#define DK_AT RA(N2)        // @
-#define DK_PND RA(N3)       // £
-#define DK_DLR RA(N4)       // $
-#define DK_EURO RA(N5)      // €
-#define DK_LCBR RA(N7)      // {
-#define DK_LBRC RA(N8)      // [
-#define DK_RBRC RA(N9)      // ]
-#define DK_RCBR RA(N0)      // }
-#define DK_PLUS MINUS       // +
-#define DK_LCRK NUBS        //
-#define DK_RCRK LS(NUBS)    //
-#define dk_TILD RA(RBKT)    // ~
+#define DK_PIPE RA(EQUAL)    // |
+#define DK_EXLM LS(N1)       // !
+#define DK_DQUO LS(N2)       // "
+#define DK_HASH LS(N3)       // #
+#define DK_CURR LS(N4)       // ¤
+#define DK_PERC LS(N5)       // %
+#define DK_AMPR LS(N6)       // &
+#define DK_SLSH LS(N7)       // /
+#define DK_LPRN LS(N8)       // (
+#define DK_RPRN LS(N9)       // )
+#define DK_EQL LS(N0)        // =
+#define DK_QUES LS(KP_MINUS) // ?
+#define DK_ASTR PIPE         // *
+#define DK_SQT BACKSLASH     // '
+#define DK_UNDS LS(DK_MINS)  // _
+#define DK_MINS SLASH        // -
+#define DK_AT RA(N2)         // @
+#define DK_PND RA(N3)        // £
+#define DK_DLR RA(N4)        // $
+#define DK_EURO RA(N5)       // €
+#define DK_LCBR RA(N7)       // {
+#define DK_LBRC RA(N8)       // [
+#define DK_RBRC RA(N9)       // ]
+#define DK_RCBR RA(N0)       // }
+#define DK_PLUS MINUS        // +
+#define DK_LCRK NUBS         //
+#define DK_RCRK LS(NUBS)     //
+#define dk_TILD RA(RBKT)     // ~
 
 #define DK_ARNG LBRC // Å
 #define DK_OSTR SEMI // Ø
