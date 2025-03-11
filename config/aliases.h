f07f1da208e3c2c8a8cc2184e149a6e35e3a963a
dembezum <dembezuuma@gmail.com>
2025-03-11 17:37:38 +0100

    BLIND

--- config/aliases.h
+++ config/aliases.h
@@ -1,7 +1,6 @@
 // aliases.h
 // #define DK_flsh AMPS
 
-#define DK_SECT LS(DK_PIPE) // §
 #define DK_PIPE RA(EQUAL)   // |
 #define DK_EXLM LS(N1)      // !
 #define DK_DQUO LS(N2)      // "
@@ -16,20 +15,16 @@
 #define DK_QUES UNDER       // ?
 #define DK_ASTR PIPE        // *
 #define DK_SQT BSLH         // '
-#define DK_COMM LS(COMMA)   // ,
-#define DK_SCLN LS(DK_COMM) // ;
-#define DK_DOT LS(DOT)      // .
-#define DK_COLN LS(DK_DOT)  // :
 #define DK_UNDS LS(DK_MINS) // _
 #define DK_MINS FSLH        // -
-#define DK_AT RA(DK_2)      // @
-#define DK_PND RA(DK_3)     // £
-#define DK_DLR RA(DK_4)     // $
-#define DK_EURO RA(DK_5)    // €
-#define DK_LCBR RA(DK_7)    // {
-#define DK_LBRC RA(DK_8)    // [
-#define DK_RBRC RA(DK_9)    // ]
-#define DK_RCBR RA(DK_0)    // }
+#define DK_AT RA(N2)        // @
+#define DK_PND RA(N3)       // £
+#define DK_DLR RA(N4)       // $
+#define DK_EURO RA(N5)      // €
+#define DK_LCBR RA(N7)      // {
+#define DK_LBRC RA(N8)      // [
+#define DK_RBRC RA(N9)      // ]
+#define DK_RCBR RA(N0)      // }
 #define DK_LABK RBRC        // <
 #define DK_RABK LS(DK_LABK) // >
 #define DK_PLUS MINUS       // +
